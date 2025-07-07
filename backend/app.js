797afea2b32c7498d7a3c88fd1bc845fe2c86014
abraham-DBA <irootezekiel318@gmail.com>
2025-07-07 21:00:34 +0300

    Revert "Merge branch 'test'"
    This reverts commit 3c6614bc3470299503c6fb7f1a33c10c4b5c3943, reversing
    changes made to 6c10cf84114b3163ac0d9e6a5c42e4e0e525de3d.

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,10 @@ import cors from "cors";
 import { customerRoute } from "./routes/customer.js";
 import { dbConnect } from "./configs/dbConnection.js";
 
-const port = 5000;
+const port = 3000;
 const app = express();
 const db = dbConnect();
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
